a3ee69800317dbff3ff1cdcf25688ff0501dd0c2
vivek1210-github <vivek.12mails@gmail.com>
2020-07-19 01:41:53 +0530

    REMOVED NEW CODE

--- hungary.php
+++ hungary.php
@@ -13,13 +13,7 @@ if($hungry == 'yes' ) {
 
     echo "drink water";
     echo "HEAD";
-    echo "HEAD1";
-    echo "head 2";
-    echo "another line";
-    echo "TAKE IT SLOW AND EASY";
-    echo "1";
-    
-    echo "3";
+   
     
     
 }
