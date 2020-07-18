4a8778464ca589d2b23658017b75db1c391b7747
vivek1210-github <vivek.12mails@gmail.com>
2020-07-18 10:35:34 +0530

    Revert "GIT UNCOMMITED COMMAND TEST1"
    This reverts commit 29e0f73f48b7406608a5c336da3a13b8f40ddb2c.

--- README.md
+++ README.md
@@ -1,12 +1,2 @@
 # gittest
 Test repo for git commands study test
-
-
-TEST FILE CREATED
-NEW FILE CREATED
-HI
-HELLO
-
-
-GOOD
-FOOD
--- hungary.php
+++ hungary.php
@@ -5,13 +5,6 @@ $hungry = "yes";
 
 if($hungry == 'yes' ) {
     echo "eat samosa";
-    echo "eat burger";
-    echo "eat junkfood";
 } else {
     echo "do your homework";
 }
-
-
-<script>
-alert('hi');
-</script>
