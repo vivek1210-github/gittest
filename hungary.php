4e2a43fe0710962c81b17dc602e84751a65e89ba
vivek1210-github <vivek.12mails@gmail.com>
2020-07-18 11:01:04 +0530

    Thirsty Branch - First changes

--- hungary.php
+++ hungary.php
@@ -1,12 +1,15 @@
 <?php
 echo "I am hungry";
 
-$hungry = "yes";
+$hungry = "no";
 
 if($hungry == 'yes' ) {
     echo "eat samosa";
     echo "eat junkfood";
     echo "eat patty";
 } else {
-    echo "do your homework";
+
+    echo "drink water";
+    
+    
 }
