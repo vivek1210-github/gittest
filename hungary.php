3c4536e4484adef526cc4e06996702d2e8684ede
vivek1210-github <vivek.12mails@gmail.com>
2020-07-19 02:01:38 +0530

    Revert to detach 3

--- hungary.php
+++ hungary.php
@@ -8,18 +8,11 @@ if($hungry == 'yes' ) {
     echo "eat junkfood";
     echo "eat patty";
     echo "drink coke and pepsi";
-    echo "Love Banana Shakes";
-    echo "eat dosa n idly sambar";
-    echo "eat pani puri";
+  
 } else {
 
     echo "drink water";
-    echo "HEAD";
-    echo "HEAD1";
-  
-    echo "head 2";
-    echo "another line";
-    echo "another line 2";
+   
     
     
 }
